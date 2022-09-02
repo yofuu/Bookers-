a48c65ec2d9540549088e1d3ef0062356e9ec9c1
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2022-09-02 15:58:54 +0000

    [Second commit]

---
+++ db/migrate/20220902151207_create_migrates.rb
@@ -0,0 +1,10 @@
+class CreateMigrates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :migrates do |t|
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
